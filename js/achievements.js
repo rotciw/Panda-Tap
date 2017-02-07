4610c2df4049f989cbba1ed16d5e3f67ef9b755e
rotciw <wictor_x@hotmail.com>
2017-02-07 21:20:33 +0100

    Add files via upload

---
+++ js/achievements.js
@@ -0,0 +1,12 @@
+ $(document).ready(function(){
+
+   
+    $('[data-toggle="popover"]').popover({
+        placement:'auto bottom',
+        trigger:'hover',
+        id:'popover',
+       //https://gist.github.com/frostney/926660
+    }); 
+
+     
+    });
